7842bfb071e0b013f4088eaf70da417e64560db0
jamalludin <jamalludin1209@gmail.com>
2022-01-13 22:35:16 +0700

    save to db

--- App/controllers/user_auth.go
+++ App/controllers/user_auth.go
@@ -12,27 +12,26 @@ type UserController struct{}
 func (h *UserController) Register(res *gin.Context) {
 	var req routes.Register
 
-	// Body json validation
+	/*Request Body validation*/
 	if res.BindJSON(&req) != nil {
 		res.JSON(406, gin.H{"message": "name, username and password required"})
 		res.Abort()
 		return
 	}
 
-	// Search existing user
+	/*Check Username Already*/
 	var user models.User
 	if err := db.PGDB.Where(" username = ?", req.Username).First(&user).Error; err == nil {
 		res.JSON(403, gin.H{"message": "Username already exist"})
 		return
 	}
 
-	// Save to SQL database
+	/*Save To DB Posgresql*/
 	user = models.User{
 		Username: req.Username,
 		Name:     req.Name,
 	}
 	db.PGDB.Create(&user)
 
-	// Positive Response
 	res.JSON(201, gin.H{"message": "Success registration"})
 }
